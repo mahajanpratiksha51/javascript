b7c57ed99019f38e38255a00961bde6f3953dc50
mahajanpratiksha51 <mahajanpratiksha51@gmail.com>
2023-05-06 21:40:17 +0530

    assignments
    assignments

---
+++ 02_variableBasicsassig.js
@@ -0,0 +1,27 @@
+var rollNo=24;
+var familyMember=4;
+var price=200;
+console.log("*---------Number--------*")
+console.log("Roll NO:->",rollNo,"Type:->", typeof rollNo);
+console.log("FamilyMember:->",familyMember,"Type:->", typeof familyMember);
+console.log("Price:->",price,"typeof:->",typeof price);
+
+var firstName= "Pratiksha";
+var hobby="Sketching";
+var gender="Female";
+console.log("*--------String-------*");
+console.log("FirstName:->",firstName,"Type",typeof firstName);
+console.log("Hobby:->",hobby,"Type:->",typeof hobby);
+console.log("Gender:->",gender,"Type:->",typeof gender);
+ 
+var knowPython=false;
+var knowAngular=true;
+var knowHtml=true;
+console.log("*--------Boolean---------*");
+console.log("know Paytthon:->",knowPython,"Type:->",typeof knowPython);
+console.log("Know Angular:->",knowAngular,"Type:->",typeof knowAngular)
+console.log("Know Html:->",knowHtml,"Type:->",typeof knowHtml);
+console.log("*----Diffrence Between Undefine And Null Data Types------*");
+console.log("Undefine:->In this variable is declare but value is not assigned to the variable");
+console.log("Null:->in this variableis declare but value is assigned as null.");
+
---
+++ 03_typeof.js
@@ -0,0 +1,29 @@
+var bankName;
+console.log("*---------Before--------*")
+ console.log("Bank Name:->",bankName);
+ console.log("Type:->",typeof bankName);
+ var bankName='SBI Bank';
+ console.log("*---------After----------*");
+ console.log("Bank Name:->",bankName);
+ console.log("Type:->",typeof bankName);
+ var marks='90%';
+ console.log("*---------Marks variable value and type--------*")
+ console.log("Marks:->",marks);
+ console.log("Type:->",typeof marks)
+ var isWorking;
+ console.log("*---------Before---------*")
+ console.log("Is Working:->",isWorking);
+ console.log("Type:->",typeof isWorking);
+ var isWorking=true;
+ console.log("*---------After---------*")
+ console.log("Is Working:->",isWorking);
+ console.log("Type:->",typeof isWorking);
+ var totalCount="Hundred and Seven";
+ console.log("*--------Total count variable value and type----------*")
+ console.log("Total Count:->",totalCount);
+ console.log("Type:->",typeof totalCount);
+ var totalCount=107;
+ console.log("*---------Update variable total count value and type-----*")
+
+console.log("Total Count:->",totalCount);
+ console.log("Type:->",typeof totalCount);
