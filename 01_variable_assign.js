f3d1d6d7ca8b681f20b5b816690c1dcd486529f9
mahajanpratiksha51 <mahajanpratiksha51@gmail.com>
2023-05-06 17:41:56 +0530

    basic js programs
    basic js programs

---
+++ 01_variable_assign.js
@@ -0,0 +1,11 @@
+var myFullName;
+console.log(myFullName); 
+myFullName="Pratiksha Mahajan";
+console.log(myFullName);
+myFullName="Pratiksha Dnyaneshwar mahajan"
+console.log(myFullName);
+var myLove;
+myLove="I love only js";
+console.log(myLove);
+var [firstName,lastName,age,address,pincode,gender,isMarried]=["Pratiksha","Mahajan", "24", "At-Tahakali, Tal-Bhusawal ,Dist-jalgaon", "425308" ,"female","NO"]
+console.log(firstName,lastName,age,address,pincode,gender,isMarried);
