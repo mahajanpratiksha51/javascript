a42c42b1da5658e92266848047a7201a090d0ad3
mahajanpratiksha51 <mahajanpratiksha51@gmail.com>
2023-05-17 09:49:56 +0530

    assignment
    assignment

---
+++ 05_operatorAssignC.js
@@ -0,0 +1,10 @@
+var args=function(gradScore,hscScore,sscScore,candidateName){
+    var result=gradScore>=70 ||hscScore>=80 ||sscScore>=90? `Congrates! ${candidateName} you are eligible for TCS interview`:`${candidateName} Unfortunately you are not eligible for interview`;
+    console.log(`  ${result}`)
+}
+console.log(`--------------Check TCS Interview Eligibility--------------------------------`);
+args(80,86,90,"Pratiksha");
+console.log(`-------------------------------------------------------------------------------`);
+args(70,65,55,"Sushma");
+console.log(`-------------------------------------------------------------------------------`);
+args(60,79,88,"Priya");
