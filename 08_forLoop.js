dd23adc22841ad57624c99fee76390f8d94179a1
mahajanpratiksha51 <mahajanpratiksha51@gmail.com>
2023-05-24 16:35:07 +0530

    jump Statement
    jump Statement

--- 08_forLoop.js
+++ 08_forLoop.js
@@ -50,3 +50,22 @@ console.log(reverseString);
 reverseWord("JavaScript");
 reverseWord("Google Chrome");
 reverseWord("Developer");
+
+var wordLength=word.length
+for(index=0;index<wordLength;index++);
+
+
+
+var word = "JavaScript Language Of Internet";
+// a e i o u == A E I O U
+var count = 0;
+for (let index = 0; index < word.length; index++) {
+    var char = word.charAt(index); // 
+    if (char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u' || 
+        char == 'A' || char == 'E' || char == 'I' || char == 'O' || char == 'U') {
+        console.log(char);
+        count++;
+    }
+    
+}
+console.log(`Count total vowels: ${count}`);
